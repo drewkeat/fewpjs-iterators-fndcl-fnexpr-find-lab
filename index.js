6f811efabf06f099aac8f964400ef442b77eb84f
Drew Keat <thedrewkeat@gmail.com>
2021-09-22 12:59:06 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 
+function superbowlWin(array) {
+    let response = array.find(object => object.result === "W")
+    if (response) {
+        return response.year
+    } else {
+        return response
+    }
+}
